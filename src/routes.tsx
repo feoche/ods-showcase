08d28b0ed9cb25f0307f3797627b43c0d94e25fc
Francois Eoche <francois.eoche@ovhcloud.com>
2025-07-18 15:43:50 +0200

    Remove Tailwind config

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,8 @@
 import { Route } from 'react-router';
-import { ErrorBoundary } from './pages/Error.page';
 import { Home } from './pages/Home.page';
-import { Layout } from './pages/Layout.page';
 
 export const Routing = (
-  <Route ErrorBoundary={ErrorBoundary} element={<Layout />}>
+  <Route>
     <Route element={<Home />} index />
   </Route>
 );
